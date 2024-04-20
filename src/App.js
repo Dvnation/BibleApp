34064963e706feed6ea870f83182ba98129a96de
Dvnation <dvnugo@gmail.com>
2024-04-20 12:52:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Bibler } from './reserve';
+
+function App() {
+  return (
+   <>
+   <Bibler/>
+   </>
+  );
+}
+
+export default App;
